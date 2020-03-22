6ec892a824cdd6b2db995262c901186695ac9054
Jeffrey Lim <ftt.jeffrey.lim@grabtaxi.com>
2020-03-22 17:37:48 +0800

    leetcode qns

---
+++ python_leetcode_qns.py
@@ -0,0 +1,7 @@
+https://leetcode.com/problems/permutations/submissions/
+
+https://leetcode.com/problems/symmetric-tree/ 
+
+https://leetcode.com/problems/plus-one/submissions/
+
+https://leetcode.com/problems/n-ary-tree-preorder-traversal/
