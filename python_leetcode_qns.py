1dd97edf4c79551a5da62eb61d4a0fcf505d1a55
Jeffrey Lim <ftt.jeffrey.lim@grabtaxi.com>
2020-03-23 15:27:24 +0800

    add qns

--- python_leetcode_qns.py
+++ python_leetcode_qns.py
@@ -1,7 +1,13 @@
 https://leetcode.com/problems/permutations/submissions/
 
-https://leetcode.com/problems/symmetric-tree/ 
+https://leetcode.com/problems/symmetric-tree/submissions
 
-https://leetcode.com/problems/plus-one/submissions/
+https://leetcode.com/problems/plus-one/submissions/submissions
 
-https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+https://leetcode.com/problems/n-ary-tree-preorder-traversal/submissions
+
+https://www.geeksforgeeks.org/merge-sort/submissions
+
+https://leetcode.com/problems/symmetric-tree/submissions
+
+https://leetcode.com/problems/balance-a-binary-search-tree/submissions
