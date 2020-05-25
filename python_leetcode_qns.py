d4a735bc648a54d2dbeb609a8c855c7c4f7e61dd
Jeffrey Lim Zhen Yi <jeffrey_lim95@hotmail.com>
2020-05-25 16:40:55 +0800

    added another bunch of qns

--- python_leetcode_qns.py
+++ python_leetcode_qns.py
@@ -1,5 +1,7 @@
 https://leetcode.com/problems/maximum-subarray/
 
+https://leetcode.com/problems/two-sum/
+
 https://leetcode.com/problems/di-string-match
 
 https://leetcode.com/problems/first-missing-positive
@@ -58,6 +60,8 @@ https://repl.it/@JeffreyLim/minimum-number-of-flips-in-binary-string
 
 https://leetcode.com/problems/path-sum/
 
+https://leetcode.com/problems/path-sum-ii/
+
 https://leetcode.com/problems/trim-a-binary-search-tree/
 
 https://leetcode.com/problems/insert-into-a-binary-search-tree/
@@ -78,7 +82,17 @@ https://leetcode.com/problems/maximum-binary-tree-ii/
 
 https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+https://leetcode.com/problems/balanced-binary-tree/
+
 https://leetcode.com/problems/validate-binary-search-tree/
+
+https://leetcode.com/problems/find-largest-value-in-each-tree-row/lanc
+
+https://leetcode.com/problems/deepest-leaves-sum/
+
+https://leetcode.com/problems/find-mode-in-binary-search-tree
 
 https://leetcode.com/problems/sort-colors/
 
@@ -93,3 +107,61 @@ https://www.hackerrank.com/challenges/stockmax/problem
 https://leetcode.com/problems/longest-palindromic-substring/
 
 https://leetcode.com/problems/product-of-array-except-self/
+
+https://leetcode.com/problems/contiguous-array/
+
+https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+https://leetcode.com/problems/combinations/
+
+https://leetcode.com/problems/reverse-integer/
+
+https://leetcode.com/problems/destination-city/
+
+https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+https://leetcode.com/problems/find-the-duplicate-number/
+
+https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+https://leetcode.com/problems/decompress-run-length-encoded-list/
+
+https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+
+https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+https://leetcode.com/problems/jewels-and-stones/
+
+https://leetcode.com/problems/defanging-an-ip-address/
+
+https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+https://leetcode.com/problems/maximum-69-number/
+
+https://leetcode.com/problems/split-a-string-in-balanced-strings/
+
+https://leetcode.com/problems/jump-game/
+
+https://leetcode.com/problems/compare-version-numbers/
+
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+
+https://leetcode.com/problems/add-binary/
+
+https://leetcode.com/problems/add-two-numbers/
+
+https://leetcode.com/problems/break-a-palindrome/
+
+https://leetcode.com/problems/set-matrix-zeroes/
+
+https://leetcode.com/problems/minimum-size-subarray-sum
+
+https://leetcode.com/problems/move-zeroes/
+
+https://repl.it/@JeffreyLim/shortest-path-to-bank
