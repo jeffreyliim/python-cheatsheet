d3c82fbbebd4d9f5a379ae8a658fc5edd8ac15f1
Jeffrey Lim <19368718+jeffreyliim@users.noreply.github.com>
2020-06-06 18:32:31 +0800

    Update leetcode_qns.py

--- leetcode_qns.py
+++ leetcode_qns.py
@@ -2,7 +2,7 @@ https://leetcode.com/problems/maximum-subarray/
 
 https://leetcode.com/problems/valid-parentheses/
 
-https://leetcode.com/problems/valid-parenthesis-string
+https://leetcode.com/problems/valid-parenthesis-string/
 
 https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
 
@@ -10,63 +10,65 @@ https://leetcode.com/problems/two-sum/
 
 https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
 
-https://leetcode.com/problems/di-string-match
+https://leetcode.com/problems/di-string-match/
 
-https://leetcode.com/problems/first-missing-positive
+https://leetcode.com/problems/first-missing-positive/
 
 https://leetcode.com/problems/palindrome-partitioning/
 
-https://leetcode.com/problems/middle-of-the-linked-list
+https://leetcode.com/problems/middle-of-the-linked-list/
 
 https://leetcode.com/problems/string-without-aaa-or-bbb/
 
-https://repl.it/@JeffreyLim/Minimum-area-two-banners-cover-buildings
+https://repl.it/@JeffreyLim/Minimum-area-two-banners-cover-buildings/
 
-https://leetcode.com/problems/jump-game
+https://leetcode.com/problems/jump-game/
 
-https://leetcode.com/problems/permutations
+https://leetcode.com/problems/permutations/
 
-https://leetcode.com/problems/symmetric-tree
+https://leetcode.com/problems/symmetric-tree/
 
-https://leetcode.com/problems/maximum-depth-of-binary-tree
+https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
-https://leetcode.com/problems/minimum-depth-of-binary-tree
+https://leetcode.com/problems/minimum-depth-of-binary-tree/
 
-https://leetcode.com/problems/maximum-depth-of-n-ary-tree
+https://leetcode.com/problems/maximum-width-of-binary-tree/
 
-https://leetcode.com/problems/plus-one
+https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
 
-https://leetcode.com/problems/n-ary-tree-preorder-traversal
+https://leetcode.com/problems/plus-one/
 
-https://www.geeksforgeeks.org/merge-sort
+https://leetcode.com/problems/n-ary-tree-preorder-traversal/
 
-https://leetcode.com/problems/balance-a-binary-search-tree
+https://www.geeksforgeeks.org/merge-sort/
+
+https://leetcode.com/problems/balance-a-binary-search-tree/
 
 https://leetcode.com/problems/sort-an-array/
 
 https://leetcode.com/problems/group-anagrams/
 
-https://leetcode.com/problems/top-k-frequent-elements
+https://leetcode.com/problems/top-k-frequent-elements/
 
 https://leetcode.com/problems/kth-largest-element-in-an-array/
 
 https://leetcode.com/problems/number-of-islands/
 
-https://repl.it/@JeffreyLim/list-of-items-with-prefix
+https://repl.it/@JeffreyLim/list-of-items-with-prefix/
 
-https://repl.it/@JeffreyLim/nested-lists
+https://repl.it/@JeffreyLim/nested-lists/
 
-https://leetcode.com/problems/leaf-similar-trees
+https://leetcode.com/problems/leaf-similar-trees/
 
-https://leetcode.com/problems/longest-common-prefix
+https://leetcode.com/problems/longest-common-prefix/
 
 https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
 
-https://leetcode.com/problems/invert-binary-tree
+https://leetcode.com/problems/invert-binary-tree/
 
 https://leetcode.com/problems/lucky-numbers-in-a-matrix/
 
-https://repl.it/@JeffreyLim/minimum-number-of-flips-in-binary-string
+https://repl.it/@JeffreyLim/minimum-number-of-flips-in-binary-string/
 
 https://leetcode.com/problems/path-sum/
 
@@ -80,13 +82,13 @@ https://leetcode.com/problems/insert-into-a-binary-search-tree/
 
 https://leetcode.com/problems/search-in-a-binary-search-tree/
 
-https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree
+https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
 
-https://leetcode.com/problems/binary-tree-level-order-traversal
+https://leetcode.com/problems/binary-tree-level-order-traversal/
 
-https://leetcode.com/problems/delete-leaves-with-a-given-value
+https://leetcode.com/problems/delete-leaves-with-a-given-value/
 
-https://leetcode.com/problems/add-one-row-to-tree
+https://leetcode.com/problems/add-one-row-to-tree/
 
 https://leetcode.com/problems/maximum-binary-tree/
 
@@ -104,9 +106,13 @@ https://leetcode.com/problems/find-largest-value-in-each-tree-row/
 
 https://leetcode.com/problems/deepest-leaves-sum/
 
-https://leetcode.com/problems/find-mode-in-binary-search-tree
+https://leetcode.com/problems/find-mode-in-binary-search-tree/
 
 https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+
+https://leetcode.com/problems/binary-tree-right-side-view/
+
+https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
 
 https://leetcode.com/problems/sum-root-to-leaf-numbers/
 
@@ -114,11 +120,11 @@ https://leetcode.com/problems/sort-colors/
 
 https://leetcode.com/problems/maximum-sum-of-two-non-overlapping-subarrays/
 
-https://leetcode.com/problems/minimum-path-sum
+https://leetcode.com/problems/minimum-path-sum/
 
 https://leetcode.com/problems/unique-paths/
 
-https://www.hackerrank.com/challenges/stockmax/problem
+https://www.hackerrank.com/challenges/stockmax/problem/
 
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
@@ -182,16 +188,16 @@ https://leetcode.com/problems/break-a-palindrome/
 
 https://leetcode.com/problems/set-matrix-zeroes/
 
-https://leetcode.com/problems/minimum-size-subarray-sum
+https://leetcode.com/problems/minimum-size-subarray-sum/
 
 https://leetcode.com/problems/move-zeroes/
 
-https://repl.it/@JeffreyLim/shortest-path-to-bank
+https://repl.it/@JeffreyLim/shortest-path-to-bank/
 
 https://leetcode.com/problems/shortest-distance-to-a-character/
 
 https://leetcode.com/problems/kth-largest-element-in-an-array/
 
-https://repl.it/@JeffreyLim/number-to-currency#main.py
+https://repl.it/@JeffreyLim/number-to-currency#main.py/
 
 https://leetcode.com/problems/course-schedule/
